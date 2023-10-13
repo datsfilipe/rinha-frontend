d32afde139cb25a5777d6ca313627e57f584c7dc
datsfilipe <datsfilipe.foss@proton.me>
2023-10-13 15:34:57 -0300

    feat: home content added lul

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('upload-btn')
+const input = document.getElementById('file-input')
+
+button.addEventListener('click', () => {
+  input.click()
+})
+
+input.addEventListener('change', () => {
+})
